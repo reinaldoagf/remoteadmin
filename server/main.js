e2e3d4cf2684a37f1f2ee8ef221b832c31cb7b5d
reinaldo <reinaldoagf1@gmail.com>
2018-03-18 21:54:19 -0400

    Conexión con socket desde el cliente

--- server/main.js
+++ server/main.js
@@ -3,6 +3,17 @@ var app= express()
 var server= require('http').Server(app)
 var io= require('socket.io')(server)
 
+
+app.use(express.static('public'))
+app.get('/',function(req,res){
+	res.status(200)
+	//res.send('Hello world, remote administrator')
+})
+//Prueba de socket.io
+io.on('connection',function(socket){
+	console.log('Alguien se ha conectado con socket')
+})
+
 server.listen(8080,function(){
 	console.log('Server running in http://localhost:8080')
 })
