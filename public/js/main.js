a05b4806e21a16cc9020c0b50ead9ddb807848f7
reinaldo <reinaldoagf1@gmail.com>
2018-03-18 23:14:52 -0400

    Sala de chat

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,24 @@
-var socket = io.connect("http://localhost:8080/", {'forceNew':true})
+var socket = io.connect("http://localhost:8080/", {"forceNew":true})
+socket.on('messages',function(data){
+	console.log(data)
+	render(data)
+})
+function render(data){
+	var html=data.map(function(element,index){
+		return (`<div>
+				<stronge>${element.author}</stronge>:
+				<em>${element.message}</em>
+			</div>`)
+	}).join(' ')			
+	document.getElementById('messages').innerHTML=html
+}
+function addMessage(e){
+	var payload={
+		'author':document.getElementById('author').value,
+		'message':document.getElementById('message').value
+	}
+	document.getElementById('author').value=''
+	document.getElementById('message').value=''
+	socket.emit('chat-message',payload)
+	return false
+}
--- server/main.js
+++ server/main.js
@@ -3,15 +3,30 @@ var app= express()
 var server= require('http').Server(app)
 var io= require('socket.io')(server)
 
+//Array de mensajes
+var messages=[{
+	'id'      : 1,
+	'message' : 'Hola, primer mensaje',
+	'author'  : 'reinaldoagf'
+}]
 
 app.use(express.static('public'))
 app.get('/',function(req,res){
 	res.status(200)
-	//res.send('Hello world, remote administrator')
+	res.send('Hello world, remote client')
+})
+app.get('/admin',function(req,res){
+	res.status(200)
+	res.send('Hello world, remote administrator')
 })
 //Prueba de socket.io
 io.on('connection',function(socket){
 	console.log('Alguien se ha conectado con socket')
+	socket.emit('messages',messages)
+	socket.on('chat-message',function(data){
+		messages.push(data)
+		io.sockets.emit('messages',messages)
+	})
 })
 
 server.listen(8080,function(){
